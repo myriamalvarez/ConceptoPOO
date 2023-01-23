b168465d1417b4f0f96cee6bcad9b063bc64743e
Myriam Alvarez <myriamalvarez1006@gmail.com>
2023-01-23 17:26:26 -0300

    Salary Employe

--- Program.cs
+++ Program.cs
@@ -9,17 +9,30 @@ namespace ConceptoPOO
             Console.WriteLine("Conceptos POO");
             Console.WriteLine("=============");
              
-            try
-            {
-	            Console.WriteLine(new Date(2024, 2, 29));
-	            Console.WriteLine(new Date(1974, 9, 23));
-	            Console.WriteLine(new Date(1985, 11, 5));
-            }
-            catch (Exception error)
-{
-	
-	Console.WriteLine(error.Message);
-}
+           Employee employee1 = new CommissionEmployee()
+           {
+            Id = 2020,
+            FirstName = "Patricia",
+            LastName = "Lopez",
+            BirthDate = new Date(1995, 5, 23),
+            HiringDate = new Date(2022, 2, 5),
+            IsActive = true,
+            Sales = 320000000M,
+            CommissionPercentaje = 0.03F
+           };
+           Console.WriteLine(employee1);
+
+            Employee employee2 = new SalaryEmployee()
+           {
+            Id = 1010,
+            FirstName = "Sandra",
+            LastName = "Morales",
+            BirthDate = new Date(1990, 5, 23),
+            HiringDate = new Date(2022, 1, 15),
+            IsActive = true,
+            Salary = 1815453.45M
+           };
+           Console.WriteLine(employee2);
         }
     }
 }
---
+++ SalaryEmployee.cs
@@ -0,0 +1,17 @@
+namespace ConceptoPOO
+{
+    public class SalaryEmployee : Employee
+    {
+        public decimal Salary { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\n\tValue to pay.........: {$"{GetValueToPay():C2}", 18}";
+        }
+    }
+}
